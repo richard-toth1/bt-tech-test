19c45cb3f9b4b84cfe836af1635f8de9d2152e16
Richard Toth <Richard_Toth1@epam.com>
2023-12-08 19:49:16 +0100

    Adds file upload capabilities

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -15,4 +15,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ api/src/Factory/UserFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<User>
+ *
+ * @method        User|Proxy                     create(array|callable $attributes = [])
+ * @method static User|Proxy                     createOne(array $attributes = [])
+ * @method static User|Proxy                     find(object|array|mixed $criteria)
+ * @method static User|Proxy                     findOrCreate(array $attributes)
+ * @method static User|Proxy                     first(string $sortedField = 'id')
+ * @method static User|Proxy                     last(string $sortedField = 'id')
+ * @method static User|Proxy                     random(array $attributes = [])
+ * @method static User|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method static User[]|Proxy[]                 all()
+ * @method static User[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static User[]|Proxy[]                 findBy(array $attributes)
+ * @method static User[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class UserFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function buyer(): self
+    {
+        return $this->addState(['roles' => [User::ROLE_BUYER]]);
+    }
+
+    public function seller(): self
+    {
+        return $this->addState(['roles' => [User::ROLE_SELLER]]);
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'password' => self::faker()->password(),
+            'roles' => self::faker()->randomElements([User::ROLE_BUYER, User::ROLE_SELLER]),
+            'username' => self::faker()->userName(),
+        ];
+    }
+
+    protected static function getClass(): string
+    {
+        return User::class;
+    }
+}
