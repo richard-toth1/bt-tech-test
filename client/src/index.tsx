a9ba4bdd97d6b91b1774d15f34ab8c31dccd951c
Richard Toth <Richard_Toth1@epam.com>
2023-12-18 17:47:01 +0100

    Add routing and styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import auctionRoutes from './routes/auction';
+import bidRoutes from './routes/bid';
+import authRoutes from './routes/auth';
+import { List } from './components/auction';
+import { AuthProvider } from './context/AuthProvider';
+import './index.css'
+import NavBar from './components/auth/NavBar';
+
+const NotFound = () => (
+  <h1>Not Found</h1>
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Router basename='/'>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<List />} key="home"/>
+          { auctionRoutes }
+          { bidRoutes }
+          { authRoutes }
+          <Route element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
