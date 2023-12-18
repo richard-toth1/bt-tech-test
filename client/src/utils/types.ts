cc754858e453711c2d7cd88e36d9f6fea7c3fa04
Richard Toth <Richard_Toth1@epam.com>
2023-12-18 17:34:09 +0100

    Add common utility types/functions

---
+++ client/src/utils/types.ts
@@ -0,0 +1,28 @@
+export type TError = SubmissionError | Error | null;
+
+export interface ApiResource {
+  "@id": string;
+}
+
+export interface SubmissionErrors {
+  [p: string]: string;
+}
+
+export class SubmissionError extends Error {
+  private readonly _errors: SubmissionErrors;
+
+  constructor(message: string, errors: SubmissionErrors) {
+    super(message);
+    this._errors = errors;
+  }
+
+  public get errors(): SubmissionErrors {
+    return this._errors;
+  }
+}
+
+export enum AuctionStatus {
+  Pending = "Pending",
+  Accepted = "Accepted",
+  Rejected = "Rejected",
+}
