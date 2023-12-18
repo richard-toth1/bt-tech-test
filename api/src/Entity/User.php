723f05d68a4921b61224b786acaa466828e2db17
Richard Toth <Richard_Toth1@epam.com>
2023-12-18 18:06:56 +0100

    Update MediaObject
    Add serialization group auction:read, bid:read so username is available on the get/getCollection endpoints of Auction and Bid

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -31,7 +31,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    #[Groups(['user:read'])]
+    #[Groups(['user:read', 'auction:read', 'bid:read'])]
     private ?string $username = null;
 
     #[ORM\Column]
