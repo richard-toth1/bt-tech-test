094a9befe2a87ed84e439651a1d9646d53cedb47
Richard Toth <Richard_Toth1@epam.com>
2023-12-18 18:30:03 +0100

    Remove next.js app

--- pwa/types/MediaObject.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from "./item";
-
-export class MediaObject implements Item {
-  public "@id"?: string;
-
-  constructor(_id?: string, public contentUrl?: string) {
-    this["@id"] = _id;
-  }
-}
--- pwa/types/item.ts
+++
@@ -1,5 +0,0 @@
-export interface Item {
-  "@id"?: string;
-}
-
-export const isItem = (data: any): data is Item => "@id" in data;
